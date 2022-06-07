90763d4304cb657017cf1fdff8ec6df5110f8e17
shinnosuke0618 <s.t061825@gmail.com>
2022-06-07 17:27:14 +0000

    [add]model

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   end
   scope module: :public do
     root to: "homes#top"
-    resources :users, only: [:new, :create, :index, :show, :edit, :update]
+    resources :users, only: [:create, :index, :show, :edit, :update]
+    get 'signup' => 'users#new'
     resources :posts
 
     get 'login' => 'sessions#new'
